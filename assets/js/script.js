023dd30a1a8ddd25cf198a1f4de4ee614a152792
digitinok <75326757+digitinok@users.noreply.github.com>
2023-01-31 15:46:01 +0000

    color coding for past, current and future hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,52 @@ let currentTime = $("#currentTime");
 let date = moment();
 currentDay.text(date.format("dddd, DD MMM YYYY"));
 currentTime.text(date.format("HH:mm:ss"));
+let currentHour = parseInt(moment().format("H"));
+console.log(currentHour);
 
 myTimer = setInterval(function () {
     // count down timer every second and finish when time is 0
     currentTime.text(moment().format("HH:mm:ss"));
 }, 1000);
   
-// i
-for (let i=8; i<18; i++) {
+// using a time window from 8:00 to 18:00 in 1 hour slots
+for (let time=8; time<18; time++) {
     // create a row for each hour
     let row = $("<div></div>");
     row.addClass("row");
-    // hour as class
-    row.addClass(String(i));
+    // add hour as class
+    row.addClass(String(time));
     timeContainer.append(row);
 
-    // timer to the left hand side
-    let hourBlock = $("<div></div>");
-    hourBlock.addClass(String(i));
+    // time to the left hand side
+    let hourBlock = $("<p></p>");
+    hourBlock.addClass(String(time));
     hourBlock.addClass("col-1 hour");
-    // display the hour i
-    hourBlock.text(i+":00")
+    // display the hour time
+    hourBlock.text(time+":00")
 
-    // descrition block in the middle
+    // Appointment block in the middle
     let appointmentBlock = $("<textarea></textarea>");
-    appointmentBlock.addClass(String(i));
-    appointmentBlock.addClass("col-10 description past");
+    appointmentBlock.addClass(String(time));
+    appointmentBlock.addClass("col-10 description time-block");
 
-    let saveButton = $("<button></button>");
-    saveButton.addClass(String(i));
+    // save button to save appointment
+    let saveButton = $('<button></button>');
+    saveButton.addClass(String(time));
     saveButton.addClass("col-1 saveBtn fas fa-save");
+    saveButton.on("click", function (event) {
+        console.log(this);
+    })
 
 
     row.append(hourBlock, appointmentBlock, saveButton);
+
+    if (currentHour > time) {
+        appointmentBlock.addClass("past");
+    } else if (currentHour === time){
+        appointmentBlock.addClass("present");
+    } else {
+        appointmentBlock.addClass("future");
+    }
 }
+
