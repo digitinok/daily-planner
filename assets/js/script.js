1bf8c3579ec0ffc1e12d1f514f10092b352cab48
digitinok <75326757+digitinok@users.noreply.github.com>
2023-01-31 15:13:07 +0000

    hour slots and save button displayed on webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let timeContainer = $(".container");
+
 let currentDay = $("#currentDay");
 let currentTime = $("#currentTime");
 let date = moment();
@@ -9,3 +11,31 @@ myTimer = setInterval(function () {
     currentTime.text(moment().format("HH:mm:ss"));
 }, 1000);
   
+// i
+for (let i=8; i<18; i++) {
+    // create a row for each hour
+    let row = $("<div></div>");
+    row.addClass("row");
+    // hour as class
+    row.addClass(String(i));
+    timeContainer.append(row);
+
+    // timer to the left hand side
+    let hourBlock = $("<div></div>");
+    hourBlock.addClass(String(i));
+    hourBlock.addClass("col-1 hour");
+    // display the hour i
+    hourBlock.text(i+":00")
+
+    // descrition block in the middle
+    let appointmentBlock = $("<textarea></textarea>");
+    appointmentBlock.addClass(String(i));
+    appointmentBlock.addClass("col-10 description past");
+
+    let saveButton = $("<button></button>");
+    saveButton.addClass(String(i));
+    saveButton.addClass("col-1 saveBtn fas fa-save");
+
+
+    row.append(hourBlock, appointmentBlock, saveButton);
+}
