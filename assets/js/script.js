f61cd9e4467476e2445afc1334220518bfc60c9f
digitinok <75326757+digitinok@users.noreply.github.com>
2023-01-31 22:17:47 +0000

    notification that appoint is stored to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,43 @@
 let timeContainer = $(".container");
+//timeContainer.addClass("time-block");
+let stored = $("<p>").html('Appointment added to <code>LocalStorage</code> <i class="fa fa-check" aria-hidden="true"></i>')
+stored.attr("id", "stored")
+stored.attr("class", "hide")
+timeContainer.append(stored);
 
-let currentDay = $("#currentDay");
-let currentTime = $("#currentTime");
+let currentDayEl = $("#currentDay");
+let currentTimeEl = $("#currentTime");
 let date = moment();
-currentDay.text(date.format("dddd, DD MMM YYYY"));
-currentTime.text(date.format("HH:mm:ss"));
+currentDayEl.text(date.format("dddd, DD MMM YYYY"));
+currentTimeEl.text(date.format("HH:mm:ss"));
 let currentHour = parseInt(moment().format("H"));
+let previousHour = parseInt(moment().format("H"));
 console.log(currentHour);
 
 myTimer = setInterval(function () {
     // count down timer every second and finish when time is 0
-    currentTime.text(moment().format("HH:mm:ss"));
+    currentTimeEl.text(moment().format("HH:mm:ss"));
+    currentHour = parseInt(moment().format("H"));
+    if (currentHour !== previousHour) {
+
+        let strTestArea = `textarea.${currentHour}`;
+        $(strTestArea).addClass("present").removeClass("future");
+        strTestArea = `textarea.${previousHour}`;
+        $(strTestArea).addClass("past").removeClass("present");
+        previousHour = currentHour;
+        
+    }
 }, 1000);
+
   
 // using a time window from 8:00 to 18:00 in 1 hour slots
-for (let time=8; time<18; time++) {
+for (let time=20; time<24   ; time++) {
     // create a row for each hour
     let row = $("<div></div>");
     // add hour as class
     row.addClass(String(time));
     row.attr("id", String(time));
-    row.addClass("row");
+    row.addClass("row time-block");
     timeContainer.append(row);
 
     // time to the left hand side
@@ -33,7 +50,7 @@ for (let time=8; time<18; time++) {
     // Appointment block in the middle
     let appointmentBlock = $("<textarea></textarea>");
     appointmentBlock.addClass(String(time));
-    appointmentBlock.addClass("col-10 description time-block");
+    appointmentBlock.addClass("col-10 description");
     appointmentBlock.val(localStorage.getItem(String(time)));
 
     // save button to save appointment
@@ -46,9 +63,12 @@ for (let time=8; time<18; time++) {
         // get the text in the corresponding text area to save into local storage
         let strTestArea = `textarea.${timeId}`;
         localStorage.setItem(timeId, $(strTestArea).val());
+        stored.removeClass("hide")
+
+        const myTimeout = setTimeout(function () {stored.addClass("hide")}, 2000);
+  
 
     })
-
 
     row.append(hourBlock, appointmentBlock, saveButton);
 
